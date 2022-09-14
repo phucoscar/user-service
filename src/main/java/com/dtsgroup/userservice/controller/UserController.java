a9b38ba1c84fba82a65ae074f7031e695338bdf5
Vu Kim Phuc <phucchelsea01@gmail.com>
2022-09-15 00:24:28 +0700

    fix controller

--- src/main/java/com/dtsgroup/userservice/controller/UserController.java
+++ src/main/java/com/dtsgroup/userservice/controller/UserController.java
@@ -6,14 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController  {
-
+    private static final String CURRENT_URL = "src\\main\\resources\\static\\images";
     @Autowired
     private UserService userService;
 
@@ -23,29 +28,51 @@ public class UserController  {
     }
 
     @PostMapping
-    public User createUser(@RequestBody User user) {
-        return userService.save(user);
+    public ResponseEntity<User> createUser(@RequestParam String name,
+                                           @RequestParam String password,
+                                           @RequestParam String email,
+                                           @RequestParam MultipartFile image) throws IOException {
+        String fileLocation = new File( CURRENT_URL).getAbsolutePath() + "\\" + image.getOriginalFilename();
+        OutputStream out = new FileOutputStream(fileLocation);
+        out.write(image.getBytes());
+        out.close();
+        User user = new User();
+        user.setName(name);
+        user.setEmail(email);
+        user.setPassword(password);
+        user.setImage(image.getOriginalFilename());
+        userService.save(user);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
     }
 
     @PutMapping("{id}")
-    public ResponseEntity<User> updateUser (@PathVariable String id, @RequestBody User user) {
+    public ResponseEntity<Object> updateUser (@PathVariable String id,
+                                              @RequestParam String name,
+                                              @RequestParam String password,
+                                              @RequestParam String email,
+                                              @RequestBody MultipartFile image) throws IOException {
        Optional<User> optionalUser = userService.findById(id);
        if (!optionalUser.isPresent()) {
-           return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+           return new ResponseEntity<>("User not found",HttpStatus.NOT_FOUND);
        }
-        User updateUser = optionalUser.get();
-        updateUser.setName(user.getName());
-        updateUser.setEmail(user.getEmail());
-        updateUser.setImage(user.getImage());
-        userService.save(updateUser);
-        return new ResponseEntity<>(updateUser, HttpStatus.OK);
+       String fileLocation = new File(CURRENT_URL).getAbsolutePath() + "\\" + image.getOriginalFilename();
+       OutputStream out = new FileOutputStream(fileLocation);
+       out.write(image.getBytes());
+       out.close();
+       User updateUser = optionalUser.get();
+       updateUser.setName(name);
+       updateUser.setEmail(email);
+       updateUser.setPassword(password);
+       updateUser.setImage(image.getOriginalFilename());
+       userService.save(updateUser);
+       return new ResponseEntity<>(updateUser, HttpStatus.OK);
     }
 
     @DeleteMapping("{id}")
-    public ResponseEntity<User> deleteUser(@PathVariable String id) {
+    public ResponseEntity<Object> deleteUser(@PathVariable String id) {
         Optional<User> optionalUser = userService.findById(id);
         if (!optionalUser.isPresent()) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>("User not found", HttpStatus.NOT_FOUND);
         }
         User user = optionalUser.get();
         userService.delete(user);
